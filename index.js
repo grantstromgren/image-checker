c73160bbf551a2e61883df5f15653f017bdc0c7f
Grant Stromgren <grant@grantstromgren.com>
2020-05-13 21:20:11 -0500

    Some updates to the comments, removing debugging due to time

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Disclaimer: This is a coding sample that I created but also can be a useful tool. Feel free to use it, but know that
+ * storing images in a database will ultimately be much more performant than within a single file.
+ */
+
 const fs = require('fs');
 
 // Config items, these are alterable as desired
@@ -7,7 +12,7 @@ const config = {
 }
 
 // Log level names, these can be changed if desired but should stay as they are
-const logLevels = [ 'INFO', 'WARNING', 'DEBUG', 'CRITICAL' ]
+const logLevels = [ 'INFO', 'WARNING', 'CRITICAL' ]
 
 // Helper methods
 const helpers = {
@@ -15,7 +20,7 @@ const helpers = {
 }
 
 // Check for at least one argument
-process.argv.length <= 2 && exit('Command missing. Use --help to view current commands.', 3);
+process.argv.length <= 2 && exit('Command missing. Use --help to view current commands.', 2);
 
 // Check for --help
 process.argv.includes('--help') && help();
@@ -25,6 +30,12 @@ if (!fs.existsSync('store.db')) {
     fs.writeFileSync('store.db', '');
 }
 
+/**
+ * Imagefile is the class for all images that match the validation criteria in validate()
+ *
+ * Methods within this class will allow us to get the base64, chunk the base64, and check the strings against a data
+ * string that is provided.
+ */
 class ImageFile {
     constructor(filePath) {
         this.filePath = filePath;
@@ -35,9 +46,9 @@ class ImageFile {
     }
 
     /**
-     * checks if chunk exists in the data provided
-     * @param data
-     * @returns {boolean}
+     * Checks if chunk exists in the data provided
+     * @param data The data read from the store.db file
+     * @returns boolean
      */
     chunksExistInData(data) {
         let exists = false;
@@ -56,9 +67,9 @@ class ImageFile {
     }
 
     /**
-     * checks if the image exists in the data provided
-     * @param data
-     * @returns {boolean}
+     * Checks if the image exists in the data provided
+     * @param data The data read from the store.db file
+     * @returns boolean
      */
     existsInData(data) {
         return RegExp(this.base64.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).test(data);
@@ -66,10 +77,10 @@ class ImageFile {
 }
 
 /**
- * preloads some items for the called commands, such as validating the image or directory passed and reading the data
+ * Preloads some items for the called commands, such as validating the image or directory passed and reading the data
  * currently in the store.db file
- * @param args
- * @returns {{data: *, stats: {total: number, tally: number}, files: []}}
+ * @param args Any passed args, just in case we need them
+ * @returns { { data: *, stats: { total: number, tally: number }, files: [] } }
  */
 function preload(args = []) {
     let arg = fs.statSync(args[0]);
@@ -104,8 +115,8 @@ function preload(args = []) {
 }
 
 /**
- * flags an image or directory of images
- * @param args
+ * Flags an image or directory of images and sends the files to be stored.
+ * @param args Options included with the command
  */
 function flag(args = []) {
     let { files, stats, data } = preload(args);
@@ -124,8 +135,8 @@ function flag(args = []) {
 }
 
 /**
- * checks an image or directory of images against the current store.db
- * @param args
+ * Checks an image or directory of images against the current store.db. Information returned to screen and log file.
+ * @param args Options included with the command
  */
 function check(args = []) {
     let { files, stats, data } = preload(args);
@@ -150,9 +161,9 @@ function check(args = []) {
 
 
 /**
- * performs validation checks against the file based upon the command used.
- * @param file
- * @returns {boolean}
+ * Performs validation checks against the file based upon the command used.
+ * @param file A file by string location relative to this file.
+ * @returns boolean
  */
 function validate(file) {
     if (!helpers.checkFileExtension(file)) {
@@ -163,10 +174,10 @@ function validate(file) {
 }
 
 /**
- * stores a file's base64 in the data store. It will not store duplicate images.
- * @param file
- * @param data
- * @returns {boolean}
+ * Stores a file's base64 in the data store. It will not store duplicate images.
+ * @param file A file by string location relative to this file.
+ * @param data The data string from within the store.db
+ * @returns boolean
  */
 function store(file, data) {
     if (file.existsInData(data)) {
@@ -179,16 +190,16 @@ function store(file, data) {
         log(`File flagged: ${file.filePath}`, 0);
         return true;
     } catch(err) {
-        exit(`Error storing file: ${file.filePath}`, 3, err);
+        exit(`Error storing file: ${file.filePath}`, 2, err);
     }
 
 }
 
 /**
- * writes to the log file and screen.
- * @param message
- * @param level
- * @param err
+ * Writes to the log file and screen.
+ * @param message The message to be logged
+ * @param level The level of log to be recorded. Affects text at the start of the log message.
+ * @param err If an err exists with the log, enter it here, it will only display err if it exists
  */
 function log(message, level = 0, err = '') {
     let logMessage = logLevels[level] + ' ' + message;
@@ -203,10 +214,16 @@ function log(message, level = 0, err = '') {
     }
 }
 
+/**
+ * Exits the program, creating a log error if err exists.
+ * @param message The message to be logged
+ * @param level The level of log to be recorded. Affects text at the start of th elog message.
+ * @param err An error response to be dumped out
+ */
 function exit(message, level = 0, err = null) {
     log(message, level);
 
-    if (err && process.argv.includes('--debug')) {
+    if (err) {
         log('', 2, err)
     }
 
@@ -215,7 +232,7 @@ function exit(message, level = 0, err = null) {
 }
 
 /**
- * call for help info
+ * Calls the help info
  */
 function help() {
     switch(process.argv[2]) {
@@ -245,10 +262,10 @@ function help() {
     process.exit(1);
 }
 
-// Dynamically run the function based on the arguments passed
+// Dynamically run the function based on the argument passed. Includes the rest of the args into an array to the func.
 try {
     eval(process.argv[2])(process.argv.slice(3));
 } catch (err) {
     // An error is most likely to happen if the command is not found
-    exit(`Command not found: ${process.argv[2]}`, 3, err);
+    exit(`Command not found: ${process.argv[2]}`, 2, err);
 }
